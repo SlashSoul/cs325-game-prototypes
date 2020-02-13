38a9e87689c97bbe27cdae77b0acd40061dcd095
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-12 20:07:32 -0500

    HW1

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -4,14 +4,34 @@ window.onload = function() {
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
 
     function preload() {
-        game.load.image('sky', 'assets/sky.png')
-        game.load.image('ground', 'assets/platform.png')
-        game.load.image('star', 'assets/star.png')
-        game.load.spritesheet('dude', 'assets/dude.png', 32, 48)
+        // Load all the assets
+        game.load.image('sky', 'assets/sky.png');
+        game.load.image('ground', 'assets/platform.png');
+        game.load.image('star', 'assets/star.png');
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
 
+    /* World Building */
+    var platforms;
+
     function create() {
-        game.add.sprite(0, 0, 'star')
+        /* Create the background and platforms */
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.add.sprite(0, 0, 'sky');
+        platforms = game.add.group()
+        platforms.enableBody = true;
+
+        /* Create the ground */
+        var ground = platforms.create(0, game.world.height - 64, 'ground');
+        ground.scale.setTo(2,2);
+        ground.body.immovable = true;
+
+        /* Create two ledges */
+        var ledge = platforms.create(400, 400, 'ground');
+        ledge.body.immovable = true;
+        ledge = platforms.create(-150, 250, 'ground');
+        ledge.body.immovable = true;
+
     }
 
     function update() {
