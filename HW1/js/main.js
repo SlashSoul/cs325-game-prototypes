312b127dac274a71a03faf261581a8b4e6c4ef7d
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-12 20:54:04 -0500

    HW1

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -15,6 +15,7 @@ window.onload = function() {
     var platforms;
 
     function create() {
+        /* Worldbuilding */
         /* Create the background and platforms */
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.add.sprite(0, 0, 'sky');
@@ -31,6 +32,23 @@ window.onload = function() {
         ledge.body.immovable = true;
         ledge = platforms.create(-150, 250, 'ground');
         ledge.body.immovable = true;
+
+        /* Player */
+        // The player and its settings
+        player = game.add.sprite(32, game.world.height - 150, 'dude');
+
+        //  We need to enable physics on the player
+        game.physics.arcade.enable(player);
+
+        //  Player physics properties. Give the little guy a slight bounce.
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
+
+        //  Our two animations, walking left and right.
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+
 
     }
 
