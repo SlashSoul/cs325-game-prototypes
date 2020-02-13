e1fd8060e750edca2412163256f56d827e83a592
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-13 02:24:12 -0500

    HW1

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -35,14 +35,12 @@ window.onload = function() {
         platforms.enableBody = true;
 
         var ground = platforms.create(0, game.world.height - 64, 'ground');
-        ground.scale.setTo(3,3);
+        ground.scale.setTo(2,2);
         ground.body.immovable = true;
 
         var ledge = platforms.create(400, 400, 'ground');
         ledge.body.immovable = true;
         ledge = platforms.create(-150, 250, 'ground');
-        ledge.body.immovable = true;
-        ledge = platforms.create(300, 600, 'ground');
         ledge.body.immovable = true;
 
         /* Player */
