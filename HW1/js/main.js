19f3777e03cf8995a7876747a51bdaf86050afde
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-12 21:03:23 -0500

    HW1

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -34,21 +34,21 @@ window.onload = function() {
         ledge.body.immovable = true;
 
         /* Player */
-        player = game.add.sprite(32, game.world.height - 150, 'dude');
-        game.physics.arcade.enable(player);
-        player.body.bounce.y = 0.2;
-        player.body.gravity.y = 300;
-        player.body.collideWorldBounds = true;
+        //player = game.add.sprite(32, game.world.height - 150, 'dude');
+        //game.physics.arcade.enable(player);
+        //player.body.bounce.y = 0.2;
+        //player.body.gravity.y = 300;
+        //player.body.collideWorldBounds = true;
 
         //  Our two animations, walking left and right.
-        player.animations.add('left', [0, 1, 2, 3], 10, true);
-        player.animations.add('right', [5, 6, 7, 8], 10, true);
+        //player.animations.add('left', [0, 1, 2, 3], 10, true);
+        //player.animations.add('right', [5, 6, 7, 8], 10, true);
 
 
     }
 
     function update() {
         /* Physics - Collisions */
-        var hitPlatform = game.physics.arcade.collide(player, platforms);
+        //var hitPlatform = game.physics.arcade.collide(player, platforms);
     }
 };
