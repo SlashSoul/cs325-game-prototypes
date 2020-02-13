c3659d3283314106a6a90afff876699a4d41cc3a
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-12 23:11:04 -0500

    HW1

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -8,7 +8,9 @@ window.onload = function() {
         game.load.image('sky', 'assets/sky.png');
         game.load.image('ground', 'assets/platform.png');
         game.load.image('star', 'assets/star.png');
-
+        game.load.image('diamond', 'assets/diamond.png')
+        game.load.image('firstaid', 'assets/firstaid.png')
+        game.load.spritesheet('baddie', 'assets/baddie.png')
         game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
 
@@ -16,57 +18,87 @@ window.onload = function() {
     var platforms;
     var cursors;
     var stars;
+    var diamonds;
+    var firstaids;
+    var baddie;
     var score = 0;
+    var health = 100;
     var scoreText;
+    var healthText;
 
     function create() {
         /* Worldbuilding */
-        /* Create the background and platforms */
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.add.sprite(0, 0, 'sky');
         platforms = game.add.group()
         platforms.enableBody = true;
 
-        /* Create the ground */
         var ground = platforms.create(0, game.world.height - 64, 'ground');
         ground.scale.setTo(2,2);
         ground.body.immovable = true;
 
-        /* Create two ledges */
         var ledge = platforms.create(400, 400, 'ground');
         ledge.body.immovable = true;
         ledge = platforms.create(-150, 250, 'ground');
         ledge.body.immovable = true;
+        ledge = platforms.create(200, 400, 'ground');
+        ledge.body.immovable = true;
 
         /* Player */
-        player = game.add.sprite(32, game.world.height - 150, 'dude');
+        player = game.add.sprite(-32, game.world.height - 150, 'dude');
         game.physics.arcade.enable(player);
         player.body.bounce.y = 0.2;
         player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
 
-        //  Our two animations, walking left and right.
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-        //  Finally some stars to collect
-        stars = game.add.group();
+        /* Baddie */
+        baddie = game.add.sprite(32, game.world.height, 'baddie');
+        game.physics.arcade.enable(baddie);
+        baddie.body.bounce.y = 0.2;
+        baddie.body.gravty.y = 300;
+        baddie.body.collideWorldBounds = true;
 
-        //  We will enable physics for any star that is created in this group
+        /* Stars */
+        stars = game.add.group();
         stars.enableBody = true;
 
-        //  Here we'll create 12 of them evenly spaced apart
         for (var i = 0; i < 12; i++) {
             var star = stars.create(i * 70, 0, 'star');
             star.body.gravity.y = 300;
             star.body.bounce.y = 0.7 + Math.random() * 0.2;
         }
 
+        /* Diamonds */
+        diamonds = game.add.group();
+        diamonds.enableBody = true;
+
+        for (var i = 0; i < 12; i++) {
+          var diamond = diamonds.create(i * 20, 0, 'diamond');
+          diamond.body.gravity.y = 300;
+          diamond.body.bounce.y = 0.7 + Math.random() * 0.2;
+        }
+
+        /* First Aid */
+        firstaids = game.add.group();
+        firstaids.enableBody = true;
+
+        for (var i = 0; i < 2; i++) {
+            var firstaid = firstaids.create(i * 50, 0, 'firstaid');
+            firstaid.body.gravity.y = 300;
+            firstaid.body.bounce.y = 0.7 + Math.random() * 0.2;
+        }
+
+        /* Score */
         scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+
+        /* Health */
+        healthText = game.add.text(-16, 16, 'Health: 100', { fontSize: '32px', fill: '#000' });
 
         /* Cursors */
         cursors = game.input.keyboard.createCursorKeys();
-
 
     }
 
@@ -74,12 +106,19 @@ window.onload = function() {
         /* Physics - Collisions */
         var hitPlatform = game.physics.arcade.collide(player, platforms);
         game.physics.arcade.collide(stars, platforms);
+        game.physics.arcade.collide(diamonds, platforms);
+        game.physics.arcade.collide(firstaids, platforms);
 
         /* Collect Stars */
         game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
+        /* Collect Diamonds */
+        game.physics.arcade.overlap(player, diamonds, collectDiamond, null, this);
+
+        /* Collect First Aid */
+        game.physics.arcade.overlap(player, firstaids, collectFirstAid, null, this);
+
         /* Movement */
-        //  Reset the players velocity (movement)
         player.body.velocity.x = 0;
 
         if (cursors.left.isDown) {
@@ -105,14 +144,22 @@ window.onload = function() {
 
     }
 
-    function collectStar (player, star) {
-
-        // Removes the star from the screen
+    function collectStar(player, star) {
         star.kill();
-
-        //  Add and update the score
         score += 10;
         scoreText.text = 'Score: ' + score;
+    }
 
+    function collectDiamond(player, diamond) {
+        diamond.kill();
+        score += 100;
+        scoreText.text = 'Score: ' + score;
+
+    }
+
+    function collectFirstAid(player, firstAid) {
+        firstaid.kill();
+        health += 100;
+        healthText.text = 'Health: ' + health;
     }
 };
