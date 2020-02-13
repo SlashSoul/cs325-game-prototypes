7e60d2fa0af4152f645b8c06b1e04562d42d2792
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-12 21:23:46 -0500

    HW1

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -13,6 +13,7 @@ window.onload = function() {
 
     var player;
     var platforms;
+    var cursors;
 
     function create() {
         /* Worldbuilding */
@@ -44,11 +45,40 @@ window.onload = function() {
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+        /* Cursors */
+        cursors = game.input.keyboard.createCursorKeys();
+
 
     }
 
     function update() {
         /* Physics - Collisions */
         var hitPlatform = game.physics.arcade.collide(player, platforms);
+
+        /* Movement */
+        //  Reset the players velocity (movement)
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown) {
+            //  Move to the left
+            player.body.velocity.x = -150;
+            player.animations.play('left');
+        }
+        else if (cursors.right.isDown) {
+            //  Move to the right
+            player.body.velocity.x = 150;
+            player.animations.play('right');
+        }
+        else {
+            // Stand still
+            player.animations.stop();
+            player.frame = 4;
+        }
+
+        //  Allow the player to jump if they are touching the ground.
+        if (cursors.up.isDown && player.body.touching.down) {
+            player.body.velocity.y = -350;
+        }
+
     }
 };
