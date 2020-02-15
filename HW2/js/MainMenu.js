2019054eac9b16599f1d879f3fb93e3ed7f6fb31
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-15 00:38:37 -0500

    HW2

--- HW2/js/MainMenu.js
+++ HW2/js/MainMenu.js
@@ -13,24 +13,17 @@ GameStates.makeMainMenu = function( game, shared ) {
     return {
 
         create: function () {
-
             //	We've already preloaded our assets, so let's kick right into the Main Menu itself.
             //	Here all we're doing is playing some music and adding a picture and button
             //	Naturally I expect you to do something significantly better :)
-
-            //music = game.add.audio('titleMusic');
-            //music.play();
-
+            music = game.add.audio('titleMusic');
+            music.play();
             game.add.sprite(0, 0, 'titlePage');
-
             playButton = game.add.button( 303, 400, 'playButton', startGame, null, 'over', 'out', 'down');
-    
         },
 
         update: function () {
-
             //	Do some nice funky main menu effect here
-
         }
 
     };
