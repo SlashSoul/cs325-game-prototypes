6d6d238a041f441bd648db687bea09c2f6863a2f
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-16 20:08:43 -0500

    HW2

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -1,24 +1,25 @@
 "use strict";
 
 window.onload = function() {
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
 
-	//	Create your Phaser game and inject it into the 'game' div.
-	//	We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
-	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+    function preload() {
+        game.load.audio('GhostPain', ['assets/GhostPain.mp3', 'assets/GhostPain.ogg', 'assets/GhostPain.wav', 'assets/GhostPain.flac']);
+        game.load.image('ghost', 'assets/ghost.png');
+        game.load.image('fireball', 'assets/fireball.png');
+        game.load.image('tile', 'assets/tile.png');
+        game.load.image('enemy', 'assets/enemy.png');
+        game.load.image('enemy2', 'assets/enemy2.png');
+    }
 
-	//	Add the States your game has.
-	//	You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
+    function create() {
+        game.stage.backgroundColor = 0xC2C3C7;
+        instructions = game.add.text(game.world.centerX, game.world.centerY, 'Click to Kill all the Ghosts!', {font: '98px Arial', fill: '#FFFFFF', align: 'center'});
+        instructions.anchor.set(0.5);
+        
+    }
 
-	// An object for shared variables, so that them main menu can show
-	// the high score if you want.
-	var shared = {};
+    function update() {
 
-	game.state.add('Boot', GameStates.makeBoot(game));
-	game.state.add('Preloader', GameStates.makePreloader(game));
-	game.state.add('MainMenu', GameStates.makeMainMenu(game, shared));
-	game.state.add('Game', GameStates.makeGame(game, shared));
-
-	//	Now start the Boot state.
-	game.state.start('Boot');
-
+    }
 };
