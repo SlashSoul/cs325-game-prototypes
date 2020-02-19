26f498c447826d9006950e847b6c101bdeb9168e
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-19 17:50:02 -0500

    HW2

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -124,6 +124,7 @@ window.onload = function() {
     function spawn(number) {
         enemy = enemies.create(number*5, number, 'ghost');
         game.physics.enable(enemy, Phaser.Physics.ARCADE);
+        enemy.body.gravity.y = 200;
         enemy.body.collideWorldBounds = true;
     }
 
