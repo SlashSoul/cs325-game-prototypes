0466dc17213f58957313e8bab48499a7e1f1cf33
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-19 19:38:00 -0500

    HW2

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -135,7 +135,7 @@ window.onload = function() {
 
     function spawn(number) {
         for (var i = 0; i < number; i++) {
-            var enemy = enemies.create(Math.floor(Math.random() * 1200), game.world.height - Math.floor(Math.random() * 150), 'ghost');
+            var enemy = enemies.create(Math.floor(Math.random() * 100) + 600, game.world.height - Math.floor(Math.random() * 150), 'ghost');
             game.physics.arcade.enable(enemy);
             enemy.body.bounce.y = 0.2;
             enemy.body.gravity.y = 300;
