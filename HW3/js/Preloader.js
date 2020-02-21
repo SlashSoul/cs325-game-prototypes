49a33e1b45b088873c58e9b0da0d2a833db8935b
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-21 16:44:19 -0500

    HW3

--- HW3/js/Preloader.js
+++ HW3/js/Preloader.js
@@ -11,7 +11,7 @@ GameStates.makePreloader = function(game) {
       // These are the assets we loaded in Boot.js
       // A nice sparkly background and a loading progress bar
       background = game.add.sprite(0, 0, 'preloaderBackground');
-      preloadBar = game.add.text(game.world.centerX, game.world.centerY, 'Loading...', {font: '25px Avenir', fill: '#FFFFFF'});
+      preloadBar = game.add.text(game.world.centerY, game.world.centerX, 'Loading...', {font: '25px Avenir', fill: '#FFFFFF'});
       //preloadBar = game.add.text(350, 440, 'Loading...', {font: '25px Avenir', fill: '#FFFFFF'});
       //preloadBar = game.add.sprite(300, 400, 'preloaderBar');
 
