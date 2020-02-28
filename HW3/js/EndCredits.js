a90d4d129e19b250e88d888816264317d609ac4f
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-02-28 00:12:12 -0500

    HW3

--- HW3/js/EndCredits.js
+++ HW3/js/EndCredits.js
@@ -1,26 +1,19 @@
 "use strict";
 
-GameStates.makeGame = function(game, shared) {
-  // Create your own variables.
-  var cat = null;
-  var spaceKey = null;
+var endCredits = {
+  create: function() {
+    game.add.sprite(0, 0, 'creditsPage');
+    var instructions = game.add.text(game.world.centerX, game.world.centerY, 'Press W to return to the Main Menu');
+    instructions.anchor.setTo(0, 0);
 
-  return {
-    create: function() {
-      game.add.sprite(0, 0, 'creditsPage');
-      var instructions = game.add.text(game.world.centerX, game.world.centerY, 'Press W to return to the Main Menu');
-      instructions.anchor.setTo(0, 0);
+    var cat = game.add.sprite(100, 245, 'cat');
+  },
 
-      cat = game.add.sprite(100, 245, 'cat');
-      //spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-    },
-
-    update: function() {
-      if (this.keyboard.isDown(Phaser.Keyboard.W)) {
-        game.state.start('MainMenu');
-      }
+  update: function() {
+    if (this.keyboard.isDown(Phaser.Keyboard.W)) {
+      game.state.start('MainMenu');
     }
-    
   }
-}
+
+
+};
