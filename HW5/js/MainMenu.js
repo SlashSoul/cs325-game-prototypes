c789a4a8e71b3e3d25c33ecd8787930d29f5af98
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-04-08 18:45:15 -0400

    HW5

--- HW5/js/MainMenu.js
+++ HW5/js/MainMenu.js
@@ -19,7 +19,7 @@ BasicGame.MainMenu.prototype = {
 		this.music.play();
 
 		this.add.sprite(0, 0, 'mountains-bg');
-		this.add.text(70, 20, 'Hero', {font: '28px Ubuntu', fill: '#FFFFFF'});
+		this.add.text(this.world.centerX, this.world.centerYs, 'Hero', {font: '28px Ubuntu', fill: '#FFFFFF'});
 
 
 		this.playButton = this.add.button(303, 400, 'playButton', this.startGame, this, 'start', 'play', 'ready');
