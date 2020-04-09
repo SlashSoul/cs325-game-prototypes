9ba2e6bb3429eb15bd6d39c495adacdaf3b31775
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-04-09 07:40:23 -0400

    HW5

--- HW5/js/Game.js
+++ HW5/js/Game.js
@@ -50,7 +50,7 @@ BasicGame.Game.prototype = {
         ground = this.add.sprite(0, 436, 'ground');
         //ground = this.add.sprite(0, this.game.world.height+32, 'ground');
         ground.enableBody = true;
-        ground.body.immovable = true;
+        ground.immovable = true;
         //ground.body.static = true;
 
 
