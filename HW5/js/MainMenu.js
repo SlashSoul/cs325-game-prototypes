c12da4d5cdb83b1e8abf4951e9c695baa967b2fa
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-04-09 13:05:04 -0400

    HW5

--- HW5/js/MainMenu.js
+++ HW5/js/MainMenu.js
@@ -7,9 +7,9 @@ BasicGame.MainMenu = function(game) {
 
 BasicGame.MainMenu.prototype = {
 	create: function() {
-		//	We've already preloaded our assets, so let's kick right into the Main Menu itself.
-		//	Here all we're doing is playing some music and adding a picture and button
-		//	Naturally I expect you to do something significantly better :)
+		// We've already preloaded our assets, so let's kick right into the Main Menu itself.
+		// Here all we're doing is playing some music and adding a picture and button
+		// Naturally I expect you to do something significantly better :)
 		this.music = this.add.audio('titleMusic');
 		this.music.play();
 
@@ -20,15 +20,15 @@ BasicGame.MainMenu.prototype = {
 	},
 
 	update: function() {
-		//	Do some nice funky main menu effect here
+		// Do some nice funky main menu effect here
 
 	},
 
 	startGame: function(pointer) {
-		//	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
+		// Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
 		this.music.stop();
 
-		//	And start the actual game
+		// And start the actual game
 		this.state.start('Game');
 	}
 };
