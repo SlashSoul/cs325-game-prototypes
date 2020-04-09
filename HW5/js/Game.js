9f61ba99811585d048836e5b40891bc5dd9d3a4e
SlashSoul <48730985+SlashSoul@users.noreply.github.com>
2020-04-08 23:23:23 -0400

    HW5

--- HW5/js/Game.js
+++ HW5/js/Game.js
@@ -44,11 +44,11 @@ BasicGame.Game.prototype = {
     		this.game.physics.p2.setImpactEvents(true);
     		this.game.physics.p2.gravity.y = 250;
 
-        var ground = this.add.sprite(0, 490, 'ground');
+        var ground = this.add.sprite(0, 540, 'ground');
         ground.unbreakable = true;
         ground.name = "ground";
         this.game.physics.p2.enable(ground);
-        ground.body.static = true; 
+        ground.body.static = true;
 
 
 
